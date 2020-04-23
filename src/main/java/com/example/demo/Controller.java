f1a2fdbbf0d78b8f9462680eb548e2a71a845f4e
ankaandonova <anka.andonova@student.hu.nl>
2020-04-23 19:46:31 +0200

    update

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -1,9 +1,6 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -11,15 +8,15 @@ import java.time.format.DateTimeFormatter;
 @RestController
 public class Controller{
 
-    @RequestMapping(value = "/current-date/", method = RequestMethod.GET)
+    @RequestMapping(value = "/current-date", method = RequestMethod.GET)
     public String getCurrentDate() {
         LocalDate localDate = LocalDate.now();
-        return DateTimeFormatter.ofPattern("dd/MM/yyy").format(localDate);
+        return "De huidige datum is:" + DateTimeFormatter.ofPattern("dd/MM/yyy").format(localDate);
     }
 
-    @RequestMapping(value = "/welcome/{name}", method = RequestMethod.POST)
-    public String getName(@PathVariable String name) {
-        return name;
+    @RequestMapping(value = "/welcome", method = RequestMethod.POST)
+    public String getName(@RequestParam("name") String value) {
+        return "welcome" + value;
     }
 
 }
